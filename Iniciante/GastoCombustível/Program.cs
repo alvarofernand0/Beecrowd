339bd9664bd41e1ba2b0a7857aa20972046d17f4
alvarofernand0 <alvarofernando1324@gmail.com>
2025-04-23 15:15:30 -0300

    Primeiro commit

---
+++ Iniciante/GastoCombustível/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int tempo = int.Parse(Console.ReadLine());
+        int velocidade = int.Parse(Console.ReadLine());
+        double distancia = (double)velocidade * (double)tempo;
+        double litros = distancia / 12;
+        Console.WriteLine($"{litros:f3}");
+    }
+}
+
---
+++ Iniciante/Iniciante/Salário.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int salary = int.Parse(Console.ReadLine());
+            double hours = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"NUMBER = {number}");
+            Console.WriteLine($"SALARY = U$ {((double)salary * hours):#0.00}");
+        }
+    }
+
