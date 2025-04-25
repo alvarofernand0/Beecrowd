18617601675b63afd17db3467c0b893e0a389273
alvarofernand0 <alvarofernando1324@gmail.com>
2025-04-25 13:10:47 -0300

    adiciona .gitattributes para padronizar finais de linha e outros arquivos .cs

---
+++ BaltaRevisao/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idade; // Correto inicia com ZERO
+        int idade = 25; // Correto inicia com 25
+        var idade = 25; // Correto inicia com 25
+        var idade; // Errado
+        const int IDADE_MINIMA = 25;
+
+        var texto = "Testando";
+        Console.WriteLine(texto);
+
+    }
+}
