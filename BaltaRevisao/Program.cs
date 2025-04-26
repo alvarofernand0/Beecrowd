d63f91bf6aa142930f854a9413af587074d34feb
Alvaro <alvarofernando1324@gmail.com>
2025-04-26 20:06:03 -0300

    add if, else e for

--- BaltaRevisao/Program.cs
+++ BaltaRevisao/Program.cs
@@ -4,14 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        int idade; // Correto inicia com ZERO
-        int idade = 25; // Correto inicia com 25
-        var idade = 25; // Correto inicia com 25
-        var idade; // Errado
-        const int IDADE_MINIMA = 25;
-
-        var texto = "Testando";
-        Console.WriteLine(texto);
+        for (int i = 0; i <= 5; i++){
+            Console.WriteLine(i);
+        
+        }
 
     }
 }
