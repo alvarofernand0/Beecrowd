8bd5a7bc5a800bc0d9dfd03e8fa8bd277462895d
alvarofernand0 <alvarofernando1324@gmail.com>
2025-04-29 16:38:30 -0300

    Novo commit

--- BaltaRevisao/Program.cs
+++ BaltaRevisao/Program.cs
@@ -4,18 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        var arr = new string[2];
-        arr[0] = "Item 1";
-
-        var arr2 = arr;
-
-        Console.WriteLine(arr[0]);
-        Console.WriteLine(arr2[0]);
-
-        arr[0] = "Item 2";
-        Console.WriteLine(arr[0]);
-        Console.WriteLine(arr2[0]);
+        
     }
-    
-    
+}
+struct Product
+{
+    public Product(int id, string name, double price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+
+    public int Id;
+    public string Name;
+    public double Price;
+
+    public double PriceInDolar(double dolar)
+    { 
+        return Price * dolar;
+    }
 }
