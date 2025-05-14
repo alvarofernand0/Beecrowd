32a85c48112c88a3f850727ce0bcac0f2097516e
alvarofernand0 <alvarofernando1324@gmail.com>
2025-05-14 15:49:10 -0300

    Adicionando questões novas

---
+++ gpt5/Program.cs
@@ -0,0 +1,25 @@
+﻿// Treino de while
+
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        int soma = 0;
+        int opcao;
+        do
+        {
+            Console.WriteLine("Digite um número");
+            opcao = int.Parse(Console.ReadLine());
+            soma += opcao;
+
+
+        }while (opcao != 0);
+
+        Console.WriteLine($"A soma é {soma}");
+        Console.ReadKey();
+    }
+}
---
+++ questao-matriz/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] minhaMatriz = new int[10, 10];
+        Console.Write("Digite um número: ");
+        Console.ReadLine();
+    }
+}
