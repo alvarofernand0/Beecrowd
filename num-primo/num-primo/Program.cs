ae313b2b0c7466fe764400e15be8469d0cc13503
Alvaro <alvarofernando1324@gmail.com>
2025-05-21 00:59:24 -0300

    adicionando muitas coisas

--- num-primo/num-primo/Program.cs
+++ num-primo/num-primo/Program.cs
@@ -1,15 +1,29 @@
-﻿
-System.Console.WriteLine("Digite um número: ");
-int numero = int.Parse(Console.ReadLine());
-bool primo = true;
-int divisao = 0;
+﻿using System;
 
-for (int i = 2; i < numero; i++)
+class Program
 {
-    if (numero % i == 0)
+    static void Main()
     {
-        primo = false;
-        break;
+        System.Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine());
+        int divisores = 0;
+
+        for (int i = 1; i <= numero; i++)
+        {
+            if (numero % i == 0)
+            {
+                divisores++;
+            }
+        }
+        if (divisores == 2)
+        {
+            System.Console.WriteLine($"{numero} É Primo");
+        }
+        else
+        {
+            System.Console.WriteLine($"{numero} Não é Primo");
+        }
     }
 }
+
 
---
+++ palindromo/palindromo/Program.cs
@@ -0,0 +1,20 @@
+﻿// Teste para verificar se uma string é palíndromo
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Digite uma palavra ou frase: ");
+        string entrada = Console.ReadLine();
+        string semEspaco = entrada.Replace(" ", "").ToLower();
+        string reverso = "";
+
+        if (!entrada.Contains(string.Empty))
+        {
+            System.Console.WriteLine("Digite apenas letras!");
+        }
+        
+        for (int i = semEspaco.)
+    }
+}
