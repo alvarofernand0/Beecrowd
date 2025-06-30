9aeae62234b6377245ebc9cacabcd845c89f6faf
AlvaroFernando <alvarofernando1324@gmail.com>
2025-06-29 22:45:04 -0300

    Adicionando Beecrowd Avançado

---
+++ beecrowdAvancado/Program.cs
@@ -0,0 +1,27 @@
+﻿// Beecrowd 1017
+using System;
+
+int tempo = int.Parse(Console.ReadLine());
+int velocidade = int.Parse(Console.ReadLine());
+int consumo = 12;
+
+decimal distanciaPercorrida = tempo * velocidade;
+
+decimal litros = distanciaPercorrida / consumo;
+
+Console.WriteLine($"{litros:f3}");
+
+// Beecrowd 1019
+int totalSegundo = int.Parse(Console.ReadLine());
+
+int horas = totalSegundo / 3600;
+int minutos = totalSegundo % 3600 / 60;
+int segundos  = totalSegundo % 60;
+
+System.Console.WriteLine($"{horas}:{minutos}:{segundos}");
+
+
+
+
+
+// Beecrowd 1061
